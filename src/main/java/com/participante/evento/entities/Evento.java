1c636ed9a74a62521068b50686f84a2f1cf837ce
irunael <pha.alvarez2020@gmail.com>
2025-05-27 09:03:10 -0300

    Feat: Adicionado as entities

--- src/main/java/com/participante/evento/entities/Evento.java
+++ src/main/java/com/participante/evento/entities/Evento.java
@@ -1,5 +1,108 @@
 package com.participante.evento.entities;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_evento")
 public class Evento {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descrição;
+    private LocalDate data;
+    private String local;
+    private Integer vagas;
+    
+    @ManyToMany
+    @JoinTable(name = "tb_evento_participante", 
+        joinColumns = @JoinColumn(name = "evento_id"), 
+        inverseJoinColumns = @JoinColumn(name = "participante_id"))
+    private Set<Participante> participantes = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrição() {
+        return descrição;
+    }
+
+    public void setDescrição(String descrição) {
+        this.descrição = descrição;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public Integer getVagas() {
+        return vagas;
+    }
+
+    public void setVagas(Integer vagas) {
+        this.vagas = vagas;
+    }
+
+    public Set<Participante> getParticipantes() {
+        return participantes;
+    }
+
+    public void setParticipantes(Set<Participante> participantes) {
+        this.participantes = participantes;
+    }
+    
+    public boolean inscreverParticipante(Participante participante) {
+        if (this.vagas != null && this.vagas > 0 && !participantes.contains(participante)) {
+            participantes.add(participante);
+            this.vagas -= 1;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean cancelarInscricao(Participante participante) {
+        if(participantes.remove(participante)) {
+            this.vagas += 1;
+            return true;
+        }
+        return false;
+    }
 }
+
--- src/main/java/com/participante/evento/entities/Participante.java
+++ src/main/java/com/participante/evento/entities/Participante.java
@@ -1,5 +1,82 @@
 package com.participante.evento.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_participante")
 public class Participante {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int telefone;
+    
+    @ManyToMany(mappedBy = "participantes")
+    private Set<Evento> eventos = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public Set<Evento> getEventos() {
+        return eventos;
+    }
+
+    public void setEventos(Set<Evento> eventos) {
+        this.eventos = eventos;
+    }
+    
+    public boolean inscreverEvento(Evento evento) {
+        if (evento.inscreverParticipante(this)) {
+            eventos.add(evento);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean cancelarInscricao(Evento evento) {
+        if (eventos.remove(evento)) {
+            evento.cancelarInscricao(this);
+            return true;
+        }
+        return false;
+    }
 }
